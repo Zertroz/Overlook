99d047f9bbf7f263ee230667c7bb92c42df2f5bf
Kass Daniello <zertroz@gmail.com>
2023-03-02 17:17:26 -0500

    Finish room class

--- test/room-test.js
+++ test/room-test.js
@@ -20,8 +20,28 @@ describe('Room tests', function() {
   });
 
   it('should have a number', () => {
-    expect(room1.number).to.equal(1)
-    expect(room2.number).to.equal(2)
-    expect(room3.number).to.equal(3)
+    expect(room1.number).to.equal(1);
+    expect(room2.number).to.equal(2);
+    expect(room3.number).to.equal(3);
   })
+
+  it('should have a type', () => {
+    expect(room1.type).to.equal(roomData[0].roomType);
+  });
+
+  it('should see if the room has a bidet', () => {
+    expect(room1.bidet).to.equal(true);
+  });
+
+  it('should have a size', () => {
+    expect(room1.bedSize).to.equal('queen');
+  });
+
+  it('should have a number of beds', () => {
+    expect(room1.numBeds).to.equal(1);
+  });
+
+  it('should have a cost per night', () => {
+    expect(room1.costPerNight).to.equal(358.4);
+  });
 })
