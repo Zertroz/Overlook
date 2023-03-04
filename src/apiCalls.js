a07e0fc743b150ef1fc85c4e800a03882ba7dd8f
Kass Daniello <zertroz@gmail.com>
2023-03-04 11:52:02 -0500

    Start login functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,32 +1,28 @@
 function getAllCustomers() {
   return fetch('http://localhost:3001/api/v1/customers')
   .then(response => response.json())
-  .then(data => console.log(data))
 }
 
 function getAllRooms() {
   return fetch('http://localhost:3001/api/v1/rooms')
   .then(response => response.json())
-  .then(data => console.log(data))
 }
 
 function getAllBookings() {
   return fetch('http://localhost:3001/api/v1/bookings')
   .then(response => response.json())
-  .then(data => console.log(data))
 }
 
 function getSpecificCustomer(id) {
   return fetch(`http://localhost:3001/api/v1/customers/${id}`)
   .then(response => response.json())
-  .then(data => console.log(data))
 }
 
 function getData() {
   const allCustomers = getAllCustomers()
   const allRooms = getAllRooms()
   const allBookings = getAllBookings()
-  Promise.all([allCustomers, allRooms, allBookings])
+  return Promise.all([allCustomers, allRooms, allBookings])
 }
 
 export { getData, getSpecificCustomer }
